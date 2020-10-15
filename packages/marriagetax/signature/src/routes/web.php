e5a7397acf126bd5d896cb3c527029df1c89db8f
Arya <arya.n@vandalayglobal.com>
2020-10-15 13:05:58 +0530

    pacakge folder

--- packages/marriagetax/signature/src/routes/web.php
+++
@@ -1,9 +0,0 @@
- <?php
-    // MyVendor\contactform\src\routes\web.php
-    Route::get('signature', function(){
-    	return view('signature::signature');
-        // return 'Hello from the contact form package';
-    });
-     // Signature routes
-    // Route::get('/signature','SignatureController@index')->name('signature');
-    ?>
